f776d9dacbf891dc44b2f2c10eb4e3a0da85eaaa
Marko Man <darkobas@gmail.com>
2016-01-27 10:41:46 +0100

    Revert "enable qcom hw flag"
    This reverts commit a8b1412fedea20e86c35cc7a551a133d694f2654.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2015 The AOSParadox Project
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -45,7 +46,7 @@ COMMON_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
 COMMON_GLOBAL_CPPFLAGS += -DNO_SECURE_DISCARD
 
 # QCOM hardware
-BOARD_USES_QCOM_HARDWARE := true
+BOARD_USES_QCOM_HARDWARE := false
 
 # Audio
 BOARD_USES_ALSA_AUDIO := true
